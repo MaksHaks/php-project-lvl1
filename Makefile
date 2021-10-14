405962669fdcd5cf54766c38eb6cf0e613df9b22
MaksHaks <girin.ma@yandex.ru>
2021-10-15 01:40:43 +0300

    Step 3

--- Makefile
+++ Makefile
@@ -4,3 +4,4 @@ brain-games:
 	./bin/brain-games
 validate:
 	composer validate
+	composer dump-autoload	
---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+namespace php\project\lvl1\Cli;      
+use function cli\line;
+use function cli\prompt;
+
+function hello() 
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
