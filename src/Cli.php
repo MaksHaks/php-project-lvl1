8c517ba4d0d7da60d116ffaf810c771ecde79a7b
MaksHaks <girin.ma@yandex.ru>
2021-10-16 00:22:23 +0300

    test

--- src/Cli.php
+++ src/Cli.php
@@ -1,12 +1,13 @@
 <?php
-namespace php\project\lvl1\Cli;      
+
+namespace php\project\lvl1\Cli;
+
 use function cli\line;
 use function cli\prompt;
 
-function hello() 
+function hello()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
-
