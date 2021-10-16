7e84fadf4b1ead40f753b2cc17c870d271da8c4a
MaksHaks <girin.ma@yandex.ru>
2021-10-17 01:17:45 +0300

    add the engine

---
+++ src/Engine.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace php\project\lvl1\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+//This file is designed to handle any game from the "Brain Games".
+//This file does not generate data for the game.
+
+function runner(array $questions, array $answers, string $instruction)
+{
+    //Player greeting and briefing
+
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($instruction);
+
+    //Questioning the player and checking the answers
+
+    for ($i = 0; $i <= 2; $i++) {
+        line("Question: {$questions[$i]}");
+        $answer = prompt("Your answer");
+
+    //Checking the answer
+
+        if ($answer === $answers[$i]) {
+            line("Correct!");
+        } else {
+            line("{$answer} is wrong answer ;(. Correct answer was {$answers[$i]}.");
+            line("Let's try again, {$name}!");
+            break;
+        }
+    }
+
+    //Congratulations to the player if all rounds of the game are won.
+
+    if ($i === 3) {
+        line("Congratulations, {$name}");
+    }
+}
---
+++ src/Games/BrainEvenGame.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace php\project\lvl1\BrainEvenGame;
+
+use function php\project\lvl1\Engine\runner;
+
+//This file provide & generate pull of data for game "Brain-even" and handles it with engine.php
+//Data inlclude information about game ($instruction) and game-data ($questions and $answers)
+//This file is only for maintenance of the engine for the game.
+
+function runEvenGame()
+{
+    $instruction = 'Answer "yes" if the number is even, otherwise answer "no".';
+    $questions = ['', '', ''];
+    $answers = ['', '', ''];
+
+//Generating questions for game.
+
+    foreach ($questions as &$value) {
+        $value = rand(1, 100);
+    }
+
+// Generating answers for game.
+
+    foreach ($answers as $key => &$value) {
+        if ($questions[$key] % 2 === 0) {
+            $value = 'yes';
+        } else {
+            $value = 'no';
+        }
+    }
+
+//Run game using engine and pull of data.
+
+    runner($questions, $answers, $instruction);
+}
